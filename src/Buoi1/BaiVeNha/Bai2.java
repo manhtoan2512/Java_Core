a8f04b7bc6011a17ba55640579c4b1a83bc03106
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-01 16:11:04 +0700

    Create Bai2.java

---
+++ src/Buoi1/BaiVeNha/Bai2.java
@@ -0,0 +1,9 @@
+package Buoi1.BaiVeNha;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Chu vi hình tròn là: " + Math.PI * a * 2);
+        System.out.println("Diện tích hình tròn là: " + Math.PI * a * a);
+    }
+}
