6b36c0d12f66ec96aebf6e35cd16a8f119f69c82
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-01 16:11:02 +0700

    Create Bai1.java

---
+++ src/Buoi1/BaiVeNha/Bai1.java
@@ -0,0 +1,10 @@
+package Buoi1.BaiVeNha;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println("Chu vi hình chữ nhật là: " + (a + b) * 2 );
+        System.out.println("Diện tích hình chữ nhật là: " + a * b );
+    }
+}
