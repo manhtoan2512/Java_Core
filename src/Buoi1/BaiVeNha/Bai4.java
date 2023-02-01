3f94b75eb0284e2bf917ecd09a8d928eafcc2695
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-01 16:11:08 +0700

    Create Bai4.java

---
+++ src/Buoi1/BaiVeNha/Bai4.java
@@ -0,0 +1,8 @@
+package Buoi1.BaiVeNha;
+
+public class Bai4 {
+    public static void main(String[] args) {
+        float a = 79.6f;
+        System.out.println("Kết quả: " + Math.round((Math.sqrt(a)) * 1000) / (float) 1000);
+    }
+}
