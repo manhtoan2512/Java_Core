b190fcce78b66fe220ca1aee8b0f8dcc01eff7a7
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-01 16:11:06 +0700

    Create Bai3.java

---
+++ src/Buoi1/BaiVeNha/Bai3.java
@@ -0,0 +1,11 @@
+package Buoi1.BaiVeNha;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        int a = -15;
+        int b = 0;
+        int c = 7;
+        float d = 45.7f;
+        System.out.println("Số lớn nhất là: " + Math.max(a, Math.max(b, Math.max(c, d))));
+    }
+}
