6cb6094b03901feae8954fb9c53c215610baac4c
manhtoan2512 <121614206+manhtoan2512@users.noreply.github.com>
2023-02-01 16:10:55 +0700

    Create Bai5.java

---
+++ src/Buoi1/BaiVeNha/Bai5.java
@@ -0,0 +1,9 @@
+package Buoi1.BaiVeNha;
+
+public class Bai5 {
+    public static void main(String[] args) {
+        float a = 897f;
+        float b = 179f;
+        System.out.println("Kết quả: " + Math.round((a / b) * 1000) / (float) 1000);
+    }
+}
